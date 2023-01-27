b2b6b911d576ad1e0393ba7be4a3b02606311eaa
Garrett van Wageningen <ghvanw283@gmail.com>
2023-01-26 22:30:55 -0600

    WIP full verse parser

--- src/VP/VerseParser.fs
+++ src/VP/VerseParser.fs
@@ -7,24 +7,57 @@ open Spin
 
 type Book = Book of string
 
+type LineRange = { From: int; Through: int }
+
+let lineCount range =
+    range.Through - range.From
+
+type LineSelection =
+    | Single of int
+    | Range of {| From: int; Through: int |}
+
+
+type Verse = 
+    { Book: Book
+      Chapter: int
+      Lines: LineSelection}
+
 let regBook : Parser<Book> =
     fun input -> 
-        input
-        |> (atLeast1 letter 
-            |> Parser.map (fun letters -> 
-                Book (String (List.toArray letters))))
+        (atLeast1 letter 
+        |> map (String.Concat >> Book)) input
+
+
+let private oneOrTwo = character '1' |> orElse (character '2')
 
 
 let numberedBook : Parser<Book> =
-    parser {
-        let! num = character '1' |> Parser.orElse (character '2')
-        and! _ = character ' '
-        and! book = atLeast1 letter
-        return Book $"{num} {book}"
-    }
+    fun input ->
+        (succeed (fun num name -> Book $"{num} {name}")
+        |> apply (token oneOrTwo)
+        |> apply (word |> map String.Concat)) input
 
 
 let book : Parser<Book> =
     fun input ->
-        input
-        |> (numberedBook |> Parser.orElse regBook)
+        (numberedBook |> orElse regBook) input
+
+
+let lines : Parser<LineSelection> =
+    parser {
+        let! from = (token numeric) |> map Convert.ToInt32
+        let! _ = token (character '-')
+        let! through = numeric |> map Convert.ToInt32
+
+        return Range {| From = from; Through = through |}
+    } |> orElse (numeric |> map (Convert.ToInt32 >> Single))
+
+
+let verse : Parser<Verse> =
+    parser {
+        let! book = token book
+        let! chapter = (token numeric) |> map Convert.ToInt32
+        let! _ = token (character ':')
+        let! lines = lines
+        return { Book = book; Chapter = chapter; Lines = lines }
+    }
--- test/VP.Test/GivenABibleVerse.fs
+++ test/VP.Test/GivenABibleVerse.fs
@@ -1,6 +1,6 @@
 namespace VP.Test
 
-open FSharp.Core
+open System
 
 open Xunit
 open FsUnit.Xunit
@@ -21,7 +21,20 @@ module ``Given a Bible verse`` =
                 |> Option.get
 
             result |> should equal "1 Corinthians"
+            (String.Concat rest) |> should equal " 10:13"
 
+    module ``And the book isn't numbered`` =
+        let verse = "Hebrews 11:1"
+        
+        [<Fact>]
+        let ``When parsing the book`` () =
+            let struct (VerseParser.Book (result), rest) =
+                VerseParser.book verse
+                |> Result.toOption
+                |> Option.get
+
+            result |> should equal "Hebrews"
+            (String.Concat rest) |> should equal " 11:1"
 
 // [<Fact>]
 // let ``And the book does not start with a number`` () =

1afd3eb4c3a5d6ab1d51d20c31a6be5493852211
Garrett van Wageningen <ghvanw283@gmail.com>
2023-01-29 19:13:24 -0600

    remove some lines

--- examples/FSharp.Script/ParseVerse.fsx
+++ examples/FSharp.Script/ParseVerse.fsx
@@ -2,17 +2,18 @@
 #r "../../src/VP/bin/Debug/net6.0/VP.dll"
 
 open VP
+open Spin
 
-let verse = "1 Corinthians 10:1-13"
 
-let struct (parsed, _) = VerseParser.verse verse |> Result.toOption |> Option.get
+let parsed = Parser.run VerseParser.verse "1 Corinthians 10:1-13"
 
 let (VerseParser.Book book) = parsed.Book
+
 let chapter = parsed.Chapter
 
 let (verses, count) =
     match parsed.Lines with
-    | VerseParser.LineSelection.Single it -> (it, 1)
-    | VerseParser.LineSelection.Range it -> (sprintf "%d - %d" it.From it.Through, it.Through - it.From)
+    | VerseParser.LineSelection.Single it -> ($"{it}", 1)
+    | VerseParser.LineSelection.Range it -> ($"{it.From} - {it.Through}", it.Through - it.From + 1)
 
 printfn "Book: %s, Chapter: %d, Verse: %s, line total: %d" book chapter verses count
